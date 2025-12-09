6101f416c2e8eb88d73d5367d443c119b9d7f514
davidjung1001 <davidjung1001@gmail.com>
2025-12-08 23:00:10 -0600

    navi

--- src/app/worksheets/free-worksheets/[slug]/[moduleSlug]/[subsectionSlug]/printable-pdf/[setNumber]/page.js
+++ src/app/worksheets/free-worksheets/[slug]/[moduleSlug]/[subsectionSlug]/printable-pdf/[setNumber]/page.js
@@ -262,11 +262,31 @@ export default async function PrintableWorksheetPage({ params }) {
 
   // Paginate questions server-side
   const pages = paginateQuestions(questions);
+  const lessonPageUrl = `/worksheets/free-worksheets/${slug}/${moduleSlug}/${subsectionSlug}`;
 
   return (
     <div className='no-print'>
 
       <TopTutorToast />
+
+      <div className="no-print p-4 bg-gray-800">
+        <Link
+          href={lessonPageUrl}
+          className="inline-flex items-center text-blue-300 hover:text-white transition duration-150"
+        >
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            className="h-5 w-5 mr-1.5" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            stroke="currentColor" 
+            strokeWidth={2}
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+          </svg>
+          <span className="font-medium text-sm sm:text-base">Back to Lesson: {subsection.subsection_title}</span>
+        </Link>
+      </div>
 
       {/* Subsection Navigation Arrows */}
       <SubsectionNavigation 
